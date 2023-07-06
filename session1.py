a31e5fcdde026515157e38217a89a2780e50ab22
msa <msai.sadehi@gmail.com>
2023-07-06 10:59:26 +0330

    sadeghi

--- session1.py
+++ session1.py
@@ -1,1 +1,93 @@
-print("hello world")
+# print("hello world")
+
+
+# x = 1
+
+# نام متغیر می تواند هر ترکیبی از کاراکترهای الفبایی انگلیسی، آندراسکور و اعداد باشد با
+# این استثنا که اولین کاراکتر نمی تواند عدد باشد
+
+# name1 = 1 # ok
+# name_1 = 1 # ok
+# _name_1 = 2 # ok
+
+# 1name = 2 # error
+# name one = 12 # error
+
+# name_one = 12 # snake case
+# nameOne = 12 # cammel case
+
+
+# x = 1
+# print(type(x))
+
+# x = 1.2
+# print(type(x))
+
+# z = 1  + 2j
+# print(type(z))
+
+
+# x = "sara"
+# print(type(x))
+
+
+# x:int = 2
+# print(x)
+
+# x = True
+# print(type(x))
+# x = False
+# print(type(x))
+
+# string1 = ""
+
+# print(type(string1))
+# string2 = ''
+# print(type(string2))
+
+
+# message = 'he said "some thing"'
+# print(message)
+
+# message = "he said \"some thing\""
+# print(message)
+
+# message = "hello. \nwelcome to python class."
+# print(message)
+# message = """hello. 
+# welcome to python class."""
+# print(message)
+# message = '''hello. 
+# welcome to python class.'''
+# print(message)
+
+
+# message = "hello"
+# print(message[0], end=" ")
+# print(message[1], end=" ")
+# print(message[2], end=" ")
+# print(message[3], end=" ")
+# print(message[4])
+# print(message[5]) # error
+# print(len(message))
+# print(message[len(message) - 1])
+# print(message[-1])
+# print(message[:3])
+# print(message[:])
+
+# print(message[-1])
+# print(message[-2])
+
+
+# message = "hello"
+# print(message[2:])
+# print(message[2:5])
+# print(message[::2])
+
+# print(message[::-1])
+
+name = input("enter a name: ")
+print("hello", name)
+
+print("hello"+ " " + name)
+# print(type(name))
